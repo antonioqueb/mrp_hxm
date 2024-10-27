92b2c93451ac6a8afd67fb9fa85d98cacc154f04
Antonio Queb <antonioqueb@alphaqueb.online>
2024-10-27 17:02:14 -0600

    Up

--- models/panelhex_workorder_data.py
+++ models/panelhex_workorder_data.py
@@ -8,15 +8,14 @@ class PanelhexWorkorderData(models.Model):
     name = fields.Char(string="Nombre del Campo", required=True)
     field_type = fields.Selection([
         ('char', 'Texto'),
-        ('float', 'Número Decimal'),
-        ('integer', 'Número Entero'),
-        ('selection', 'Selección'),
-        ('many2one', 'Relación'),
-        ('json', 'JSON')
+        ('boolean', 'Booleano'),
+        ('integer', 'Número Entero')
     ], string="Tipo de Campo", required=True)
-    value = fields.Char(string="Valor")
+    value_char = fields.Char(string="Valor Texto")
+    value_boolean = fields.Boolean(string="Valor Booleano")
+    value_integer = fields.Integer(string="Valor Entero")
 
-    @api.depends('name', 'field_type')
+    @api.depends('name')
     def _compute_field_description(self):
         for record in self:
             if record.name:
@@ -26,24 +25,44 @@ class PanelhexWorkorderData(models.Model):
 
     field_description = fields.Char(string="Descripción del Campo", compute='_compute_field_description', store=True)
 
+    @api.depends('field_type', 'value_char', 'value_boolean', 'value_integer')
+    def _compute_value(self):
+        for record in self:
+            if record.field_type == 'char':
+                record.value = record.value_char
+            elif record.field_type == 'boolean':
+                record.value = str(record.value_boolean)
+            elif record.field_type == 'integer':
+                record.value = str(record.value_integer)
+
+    value = fields.Char(string="Valor", compute='_compute_value', store=True)
+
+    @api.onchange('field_type')
+    def _onchange_field_type(self):
+        if self.field_type == 'char':
+            self.value_boolean = False
+            self.value_integer = 0
+        elif self.field_type == 'boolean':
+            self.value_char = ''
+            self.value_integer = 0
+        elif self.field_type == 'integer':
+            self.value_char = ''
+            self.value_boolean = False
+
     def get_value(self):
         self.ensure_one()
-        if self.field_type == 'float':
-            return float(self.value) if self.value else 0.0
+        if self.field_type == 'char':
+            return self.value_char
+        elif self.field_type == 'boolean':
+            return self.value_boolean
         elif self.field_type == 'integer':
-            return int(self.value) if self.value else 0
-        elif self.field_type == 'many2one':
-            return int(self.value) if self.value else False
-        elif self.field_type == 'json':
-            import json
-            return json.loads(self.value) if self.value else {}
-        else:  # char, selection
-            return self.value
+            return self.value_integer
 
     def set_value(self, val):
         self.ensure_one()
-        if self.field_type == 'json':
-            import json
-            self.value = json.dumps(val)
-        else:
-            self.value = str(val)
+        if self.field_type == 'char':
+            self.value_char = val
+        elif self.field_type == 'boolean':
+            self.value_boolean = bool(val)
+        elif self.field_type == 'integer':
+            self.value_integer = int(val)
