d8db349ff319d2e7537459bfe3c32b97512f6ea3
Antonio Queb <antonioqueb@alphaqueb.online>
2024-11-03 13:36:10 -0600

    Up Stable Version

--- models/mrp_workcenter.py
+++ models/mrp_workcenter.py
@@ -5,8 +5,3 @@ class MrpWorkcenter(models.Model):
     _inherit = 'mrp.workcenter'
 
     equipo_id = fields.Many2one('panelhex.equipo', string='Equipo Hexamex')
-    allowed_employee_ids = fields.Many2many(
-        'hr.employee',
-        string='Empleados Autorizados',
-        help='Lista de empleados que pueden acceder a este centro de trabajo'
-    )
